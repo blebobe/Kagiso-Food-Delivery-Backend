befa951eb76909014dfcf8624c4cf85041c27157
blebobe <blebobe@live.com>
2025-11-28 12:14:25 +0200

    Delete routes/releaseRoutes.js

--- routes/releaseRoutes.js
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import { getReleaseForIdentifier } from "../controllers/releaseController.js";
-const router = express.Router();
-router.get("/", getReleaseForIdentifier);
-export default router;
